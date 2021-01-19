ecc3055eba8d8e51217c12a5fa9d84c5bf8ef77d
Louis Irwin <coding@louisirwin.co.uk>
2021-01-19 17:27:14 +0000

    🚀 Added dist files

---
+++ dist/server/server.py
@@ -0,0 +1,25 @@
+import os
+import http.server
+import socketserver
+
+# Determine the port number
+if 'IN_PROCESS_PORT' in os.environ:
+  PORT = os.environ['IN_PROCESS_PORT']
+  print("IN_PROCESS_PORT found:", PORT)
+else:
+  PORT = 8006
+  print("IN_PROCESS_PORT not found. Using port", PORT)
+
+# Define the request handler
+class ClientRequestHandler(http.server.SimpleHTTPRequestHandler):
+  def do_GET(self):
+    self.path = f"client{self.path}"
+    return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+# Instantiate the request handler
+Handler = ClientRequestHandler
+
+# Begin the webserver
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at port", PORT)
+    httpd.serve_forever()
